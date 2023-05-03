ad947d277666c0d9b28393f6353af30b4c9c9e03
Alisa <alisa.luoma@hotmail.fi>
2023-05-03 13:20:07 +0300

    Updating App

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # auth-security
+
+
+### For run
+npm run
+
+### For run Nodemon
+nodemon app.js
+
+## Start
+
+npm init -y 
+
+### Install packages
+
+npm i express ejs body-parser
+
+npm mongoose
--- app.js
+++ app.js
@@ -1,1 +1,75 @@
 //jshint esversion:6
+const bodyParser = require("body-parser");
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app =  express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB",{useNewUrlParser: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+app.get("/", (req, res)=> {
+    res.render("home");
+});
+
+app.get("/login", (req, res)=> {
+    res.render("login");
+});
+
+app.get("/register", (req, res)=> {
+    res.render("register");
+});
+
+
+app.post("/register", (req,res)=>{
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+ 
+    newUser.save()
+    .then(function(){
+        res.render("secrets");
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+});
+
+app.post("/login", (req, res)=> {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username})
+    .then(function(foundUser) {
+        if(foundUser.password === password){
+            res.render("secrets");
+        }
+    })
+    .catch(function(err) {
+        console.log(err);
+    })
+ 
+});
+
+
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
